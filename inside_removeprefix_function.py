244b76056dc2dc0ead7b4f703964e37c6fe800df
mob99-9 <jamesorozco20050@gmail.com>
2025-03-26 18:48:05 +0800

    remove_prefix_function

---
+++ inside_removeprefix_function.py
@@ -0,0 +1,15 @@
+#Prog02. removeprefix() remove the characters at the beginning of the string that matches the function 
+# parameter. Create a program that do the same functionality without using removeprefix() function.
+
+#ask user for a string and the letters the user wants to remove
+
+string = input('Please enter a string: ')
+
+remove_letter = input('From the beginning, what letter/s would you like to remove?: ')
+
+#check in the letter is in the string 
+
+try:
+    print(string.lstrip(remove_letter))
+except:
+    print(f'{remove_letter} is not in {string}')
